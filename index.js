d0eb1715106d0889119cf58bb8d5ea60ecd0d8ab
Pavel Sergienko <pavelsergienko7@gmail.com>
2023-04-09 19:52:25 +0300

    add image, create loop for data in js

--- index.js
+++ index.js
@@ -6,5 +6,17 @@ import str from './data.js'
 // console.log(str.match(/^Ответ.*$/gim))
 // Массив ответов
 
-let questions = str.match(/.*\?$/gim)
-let answers = str.match(/^Ответ.*$/gim)
+const rootDiv = document.querySelector('#root')
+const questions = str.match(/.*\?$/gim)
+const answers = str.match(/^Ответ.*$/gim)
+const len = questions.length
+
+for (let i = 0; i < len; i++) {
+	const h4 = document.createElement('h4')
+	const p = document.createElement('p')
+
+	h4.innerHTML = `${i + 1}. ` + questions[i]
+	p.innerHTML = answers[i]
+	rootDiv.append(h4)
+	rootDiv.append(p)
+}
