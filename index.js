0a8f452be829de4cc476c828df9e63105be88efa
Pavel Sergienko <pavelsergienko7@gmail.com>
2023-04-09 20:33:13 +0300

    add functions createArrData & createTextBlock

--- index.js
+++ index.js
@@ -9,14 +9,43 @@ import str from './data.js'
 const rootDiv = document.querySelector('#root')
 const questions = str.match(/.*\?$/gim)
 const answers = str.match(/^Ответ.*$/gim)
-const len = questions.length
+const dataArr = []
 
-for (let i = 0; i < len; i++) {
-	const h4 = document.createElement('h4')
-	const p = document.createElement('p')
-
-	h4.innerHTML = `${i + 1}. ` + questions[i]
-	p.innerHTML = answers[i]
-	rootDiv.append(h4)
-	rootDiv.append(p)
+function createArrData() {
+	const len = questions.length
+	for (let i = 0; i < len; i++) {
+		const newObj = {
+			0: questions[i],
+			1: answers[i],
+		}
+		dataArr.push(newObj)
+	}
 }
+
+createArrData()
+
+/////////////////
+
+function createTextblock(arr) {
+	for (let item of arr) {
+		const h4 = document.createElement('h4')
+		const p = document.createElement('p')
+		h4.innerHTML = item[0]
+		p.innerHTML = item[1]
+		rootDiv.append(h4)
+		rootDiv.append(p)
+	}
+}
+createTextblock(dataArr)
+
+function pagination() {
+	let currentPage = 1
+	let rows = 3
+
+	const start = rows * (currentPage - 1)
+	const end = start + rows
+	const paginatedData = dataArr.slice(start, end)
+	// console.log(paginatedData)
+}
+
+pagination()
